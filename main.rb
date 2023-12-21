cda5a72b0875dadadb8fa9845bc86641040075d0
Yhoselin Sanchez <yhoselin.sap@protonmail.com>
2023-12-21 14:44:09 -0600

    create vigenere class

--- main.rb
+++ main.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
+require_relative 'vigenere'
+
 puts 'que deseas hacer?'
 puts '1.- codificar un mensaje'
 puts '2.- decodificar un mensaje'
 selection = $stdin.gets.chomp.to_i
 
+cipher = Vigenere.new
 if selection == 1
   puts 'codificando'
+  puts 'escribe una clave:'
+  key = $stdin.gets.chomp
+  puts 'escribe tu mensaje:'
+  message = $stdin.gets.chomp
+
+  cipher.encode(key, message)
 elsif selection == 2
   puts 'decodificando'
+  puts 'escribe tu clave'
+  key = $stdin.gets.chomp
+  puts 'escribe tu mensaje cifrado'
+  message = $stdin.gets.chomp
+
+  cipher.decode(key, message)
 else
   puts 'Elije una opcion valida'
 end
---
+++ vigenere.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Vigenere
+  def encode(key, message)
+    puts "tu clave es: #{key}"
+    puts "tu mensaje es: #{message}"
+  end
+
+  def decode(key, message)
+    puts "tu clave es: #{key}"
+    puts "el mensaje a descifrar es: #{message}"
+  end
+end
