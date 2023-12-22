3abff83e75b404ba61396d8c1ce2bcd4eaf24aca
Yhoselin Sanchez <yhoselin.sap@protonmail.com>
2023-12-22 13:30:59 -0600

    create logic for encoding message

--- vigenere.rb
+++ vigenere.rb
@@ -1,9 +1,39 @@
 # frozen_string_literal: true
 
 class Vigenere
+  def initialize
+    @abc = ('A'..'Z').to_a
+    @abc.insert(13, 'Ñ')
+  end
+
   def encode(key, message)
     puts "tu clave es: #{key}"
     puts "tu mensaje es: #{message}"
+    key = key.upcase
+    message = message.upcase
+    tokens = message.split(' ')
+    tokens_encoded = []
+
+    key_index = 0
+    tokens.each do |token|
+      tokens_encoded.push(cypher(key, token, key_index))
+      key_index = token.length % key.length
+    end
+
+    message_endcoded = tokens_encoded.join(' ')
+    puts "Mensaje codificado: #{message_endcoded}"
+  end
+
+  def cypher(key, token, key_index)
+    numeric_values = []
+    token.chars.each do |char|
+      key_char_value = @abc.find_index(key[key_index])
+      token_char_value = @abc.find_index(char)
+      char_value_encoded = (key_char_value + token_char_value) % @abc.length
+      numeric_values.push(char_value_encoded)
+      key_index = (key_index + 1) % key.length
+    end
+    numeric_values
   end
 
   def decode(key, message)
